22d2b2aee51329abb1353cf3be633fd29a55a3ae
DaanWesterhof <daan.westerhof@student.hu.nl>
2021-09-11 02:04:41 +0200

    Improved tokanizer to keep strings intact with spaces and quatation marks
    added weird printing function

--- CookingScript.py
+++ CookingScript.py
@@ -6,6 +6,13 @@ keywordslist = [
     "->"
 ]
 
+types = [
+    "liter",
+    "milliliter",
+    "eggs",
+    "cheese"
+]
+
 def remove_useless(list):
     if len(list) == 0:
         return []
@@ -17,18 +24,59 @@ def remove_useless(list):
 
 def subsplit(list):
     if len(list) == 1:
-        return remove_useless(re.split('([\n:.,()])', list[0]))
+        return remove_useless(re.split('([\n:.,()"])', list[0]))
     else:
-        return remove_useless(re.split('([\n:.,()])', list[0]) + subsplit(list[1:]))
+        return remove_useless(re.split('([\n:.,()"])', list[0]) + subsplit(list[1:]))
+
+
+
+def fixingString(list):
+    if len(list) == 0:
+        return []
+    if list[0] != '"' and '\\' not in list[0]:
+        if len(list) >= 2:
+            x = fixingString(list[1:])
+            if x[0] != '"':
+                return [list[0] + x[0]] + x[1:]
+            else:
+                return [list[0]] + x
+        else:
+            return [list[0]] + fixingString(list[1:])
+    elif list[0] == '"':
+        return [list[0]] + fixStrings(list[1:])
+    elif list[0] == "\\":
+        if list[1] == '"':
+            x = fixingString(list[2:])
+            return ['"' + x[0]] + x[1:]
+    elif "\\" in list[0]:
+        if list[1] == '"':
+            x = fixingString(list[2:])
+            return [list[0][:-2] + '"'] + x
+
+
+def fixStrings(list):
+    if len(list) > 0:
+        if list[0] == '"':
+            return [list[0]] + fixingString(list[1:])
+        elif list[0] != " ":
+            return [list[0]] + fixStrings(list[1:])
+        else:
+            return fixStrings(list[1:])
+    else:
+        return []
+
 
 
 def tokanizer(input):
-    return subsplit(input.split(" "))
+    val = subsplit(re.split("( )", input))
+    print(val)
+    return fixStrings(val)
 
 def lexer():
     f = open("main.cook", "r")
     tokens = tokanizer(f.read())
     print(tokens)
+    return tokens
 
 
 
@@ -68,7 +116,41 @@ def defineFunction(tokens):
     new_function.name = tokens[0]
 
 
+def verifyPrint(tokens, index) -> ([], bool, str):
+    if index == 0:
+        if tokens[0] == "(":
+            return verifyPrint(tokens[1:], index+1)
+        else:
+            return tokens[1:], False, ""
+    elif index == 1:
+        if tokens[0] == '"': #or it is a variable that we can print
+            return verifyPrint(tokens[1:], index+1)
+    else:
+        if tokens[0] == "\\":
+            if tokens[1] == '"':
+                return verifyPrint(tokens[2:], index+2)
+        if tokens[0] == '"':
+            if tokens[1] == ')':
+                return (tokens, True, "")
 
+        else:
+            x, y, z = verifyPrint(tokens[1:], index+1)
+            z = tokens[0] + z
+            return (x, y, z)
+
+
+
+
+
+
+
+def recursiveParse(tokens):
+    if len(tokens) > 0:
+        if tokens[0] == "taste":
+            tokens, good, tekst = verifyPrint(tokens[1:], 0)
+            if good:
+                print(tekst)
+        recursiveParse(tokens[1:])
 
 
 
@@ -81,4 +163,5 @@ def parser(tokens, program_class: Program):
 
 
 
-lexer()
+token = lexer()
+recursiveParse(token)
