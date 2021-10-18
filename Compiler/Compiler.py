6a9e4ed6987dff5d482ab225b194758cde62eabe
DaanWesterhof <daan.westerhof@student.hu.nl>
2021-10-18 22:48:38 +0200

    added work for loops and if statements

--- Compiler/Compiler.py
+++ Compiler/Compiler.py
@@ -5,6 +5,16 @@ from typing import *
 from ErrorHandler.ErrorHandler import *
 
 
+comparator_operator_dict = {
+    "<" : "bge",
+    ">" : "ble",
+    "<=": "bgt",
+    ">=": "blt",
+    "!=": "beq",
+    "==": "bne"
+}
+
+
 def evaluate_operator(operator, a, b) -> AST_Node:
     if operator == '+':
         return AST_Integer(a.value + b.value)
@@ -27,6 +37,7 @@ def evaluate_operator(operator, a, b) -> AST_Node:
         return AST_Integer(int(a > b))
     elif operator == "<":
         return AST_Integer(int(a < b))
+
 
 def generateOperatorString_var_var_int(operator, )
 
@@ -64,12 +75,15 @@ def formatCodeLine(operator, start_reg, value):
         str_this = "mul r{}, r{}, {}\n".format(start_reg, start_reg, value)
     return str_this
 
+
 def composeCalculation(node, var_dict, start_reg = 0) -> str:
     if isinstance(node.left, AST_Operator) and isinstance(node.right, AST_Operator):
         str_left = composeCalculation(node.left, var_dict, start_reg)
         str_right = composeCalculation(node.right, var_dict, start_reg + 1)
         str_this = formatCodeLine(node.operator, start_reg, "r{}".format(start_reg+1))
         return str_left + str_right + str_this
+
+
     elif isinstance(node.left, AST_Operator):
         str_left = composeCalculation(node.left, var_dict, start_reg)
         if isinstance(node.right, AST_Literal):
@@ -86,21 +100,65 @@ def composeCalculation(node, var_dict, start_reg = 0) -> str:
             return str_right + formatCodeLine(node.operator, start_reg, var_dict[node.left.name])
 
 
-
 def compileAssignment(node, var_dict):
     if isinstance(node.left, AST_Variable):
         var_dict[node.left.name] = "r"
         var_dict[node.left.name] = "r" + str(3 + len(var_dict))
     simple_node = simplifyNode(node.right)
     if isinstance(simple_node, AST_Integer):
-        return "MOV " + var_dict[node.left.name] + ", " + str(simple_node.value)
+        return "movs    " + var_dict[node.left.name] + ", " + str(simple_node.value)
     else:
-        code_string =
+        code_string = composeCalculation(node.right, var_dict, 0)
+        return code_string + "movs    " + var_dict[node.left.name] + ", r0\n"
+
+        ldr     r3, [r7, #12]
+        cmp     r3, #29
+        bgt     .L2
+
+
+def compileLoop(node: AST_Loop, var_dict, depth, count):
+    if isinstance(node.condition, AST_RelationalOperators):
+        operator_string = ""
+        passable_par1 = ""
+        passable_par2 = ""
+        if isinstance(node.condition.left, AST_Operator):
+            calc_l = composeCalculation(node.condition.left, var_dict, 0)
+            operator_string += calc_l
+            passable_par1 = "r0"
+        elif isinstance(node.condition.left, AST_Literal):
+            passable_par1 = "#" + str(int(node.condition.left.value))
+        elif isinstance(node.condition.left, AST_VariableReference):
+            passable_par1 = var_dict[node.condition.left.name]
+
+        if isinstance(node.condition.right, AST_Operator):
+            calc_2 = composeCalculation(node.condition.right, var_dict, 0)
+            operator_string += calc_2
+            passable_par2 = "r1"
+        elif isinstance(node.condition.right, AST_Literal):
+            passable_par2 = "#" + str(int(node.condition.right.value))
+        elif isinstance(node.condition.right, AST_VariableReference):
+            passable_par2 = var_dict[node.condition.right.name]
+
+
+
+        strings = "cmp     {}, {}\n" \
+                  "{}     {}".format(passable_par1, passable_par2, comparator_operator_dict[node.condition.operator], label)#if its not good skip the loop
+    elif isinstance(node.condition, AST_Bool):
+        if node.condition.value:
+            pass #do the loop until return
+        else:
+            pass #do not compile codeblock but give warning
+    else:
+
+
+    #create start loop label,
+    #create condition
+    #create codeblock
 
 
 def compileCodeBlock(code_sequence: [AST_Node], var_dict) -> str:
         if isinstance(code_sequence[0], AST_AssignmentOperator):
-            pass
+            val = compileAssignment()
         elif isinstance(code_sequence[0], AST_Loop):
             pass
         elif isinstance(code_sequence[0], AST_IfStatement):
@@ -109,11 +167,6 @@ def compileCodeBlock(code_sequence: [AST_Node], var_dict) -> str:
             pass
         elif isinstance(code_sequence[0], AST_PrintFunctionCall):
             pass
-
-
-
-
-
 
 
 def startFunctionCodeBlock(func: AST_Function):
